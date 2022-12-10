fea1f0b8fc6bc87d59798f7e10dc8b6b7ef0a2d9
KonstantinosKotsias <115108409+KonstantinosKotsias@users.noreply.github.com>
2022-12-10 18:30:28 +0200

    Add files via upload

---
+++ Like.java
@@ -0,0 +1,14 @@
+package uniQ;
+
+public class Like extends Message {
+	private int numberOfLikes;	
+	public int likeCounter(int numberOfLikes) {
+		return numberOfLikes;
+	}
+	private int newLike(int numberOfLikes) {
+		return ++numberOfLikes;
+	}
+	private int takeLikeBack(int numberOfLikes) {
+		return --numberOfLikes;
+	}
+}

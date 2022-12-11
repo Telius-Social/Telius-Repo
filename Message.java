6246b87609b26fc57165946270f7bb5be241da50
mikegiannoulis <116588520+mikegiannoulis@users.noreply.github.com>
2022-12-11 16:43:58 +0200

    Add files via upload

--- Message.java
+++ Message.java
@@ -1,45 +1,64 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.io.Writer;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Message { // if Message extended User, we would be able to go without some extra code and also use the inherited fields without re-reading/ checking/ writing them
-	public static void chat(String username1,String username4) { // for example, username1 (the user's username) could have been inherited by the User class
-		ArrayList<String> messages=new ArrayList<String>(); 
-		boolean stop=false;
-		System.out.println("Enter first username: "); // why do we need to re-learn and re-check the sender's username if they have already logged in?
-		Scanner input1=new Scanner(System.in);
-		username1=input1.next();
-		System.out.println("Enter second username:  "); 
-		Scanner input2=new Scanner(System.in);
-		username4=input2.next();
-		int index1=0;
-		int index2=0;
-		for(int i=0;i<User.usernames.size();i++) {
-			if(username1==User.usernames.get(i)) {
-				index1=i;
+public class Message {
+	public String username;
+	public String message;
+	public Message(String usern,String str) {
+		this.username=usern;
+		this.message=str;
+	}
+	public String toString() {
+		return String.format("%s: %s",username,message );
+	}
+	public static void chat() throws FileNotFoundException, IOException {
+		Scanner input = new Scanner(System.in);
+		BufferedWriter out = new BufferedWriter(new FileWriter("C:\\Users\\User\\Desktop\\bechat.txt"));
+		BufferedReader in = new BufferedReader(new FileReader("C:\\Users\\User\\Desktop\\bechat.txt"));
+        System.out.println("Enter your username");	
+		String username1=input.next();
+		String username2;
+		boolean count=true;
+		try {
+		while(count==true) {
+				System.out.println("Press 1 to send a message or 2 to see your messages");
+				int answer=input.nextInt();
+				if(answer==1) {
+					System.out.println("Enter the user you want to send the message:");
+					username2=input.next();
+					System.out.println("Enter the message:");
+					String mes=input.next();
+					sendAmessage(out,mes);	
+				}
+				if(answer==2) {
+					seeAmessage(in,username1);
+				}
+				System.out.println("Enter true if you want to stop:");
+				count=input.hasNextBoolean();
 			}
-			if(username4==User.usernames.get(i)) {
-				index2=i;
-			}
+		} catch (InputMismatchException e) {
+			System.out.println("You have to enter either 1 or 2: ");
 		}
-		while(stop==false) {
-			System.out.print(User.usernames.get(index1)+" enter message:"); // needs try catch block (at least 1 char, no more than 1000 chars or smth)
-			// messageID
-			String message1=input1.next();
-			System.out.println(User.usernames.get(index1)+":"+message1);
-			System.out.print("|||||"+
-					          "||||");
-			String messageID=
-			System.out.print(User.usernames.get(index2));
-			System.out.print(" enter message:");
-			String message2=input2.next();
-			System.out.println(User.usernames.get(index2)+":"+message2);
-			System.out.print("|||||"+
-			          "||||");
-			System.out.println("enter 1 to stop, 2 to continue: ");
-			int sit=input1.nextInt();		
-			if(sit==1) {
-			stop=true;
-		}
-		}
-}
+		
+	}
+	public static void sendAmessage(BufferedWriter fis,String wer) throws IOException {
+		fis.write(wer);
+		fis.close();
+	}
+	public static void seeAmessage(BufferedReader tre,String x) throws IOException {
+		System.out.println(x+"sent you the message:"+":"+tre.readLine());
+	}
 }

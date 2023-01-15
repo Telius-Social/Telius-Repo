431a0d2aeb35e7d02831c21ba7e87b32e1698545
mikegiannoulis <116588520+mikegiannoulis@users.noreply.github.com>
2023-01-15 17:22:23 +0200

    Update Message.java

--- Message.java
+++ Message.java
@@ -9,14 +9,16 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.io.Writer;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Message {
-	public static String  receiver;
-	public static ArrayList<String> messages=new ArrayList<String>();
+	public static String receiver;
+	public static ArrayList<String> messages = new ArrayList<String>();
+
 	public static void sendAmessage() throws IOException {
 		Scanner input = new Scanner(System.in);
 		FileWriter fis = new FileWriter("C:\\Users\\User\\Desktop\\chat.txt", true);
@@ -31,40 +33,42 @@ public class Message {
 			}
 		}
 		System.out.print("Enter the user you want to send the message: ");
-		receiver=input.next();
+		receiver = input.next();
 		boolean value = Files.checkRegister(receiver, Files.usernames, Files.dataUsernames);
-		while(value==false) {
+		while (value == false) {
 			System.out.println("This user does not exist");
 			System.out.print("Enter again the username:");
-			receiver= input.next();
+			receiver = input.next();
 			value = Files.checkRegister(receiver, Files.usernames, Files.dataUsernames);
 		}
-		if(value==true) {
-			System.out.println("Enter the message you want to send: ");
-			String mess=input.next();
-			mess=String.format("From:%s%nTo:%s%nMessage:%s",UserExperience.username,receiver,mess);
-			if(UserExperience.username!=null) {
-				in.write(mess);
-				in.newLine();
-				in.close();
+		if (value == true) {
+			Scanner enter = new Scanner(System.in);
+			System.out.print("Enter a message:");
+			String message = enter.nextLine();
+			String mess = String.format("From:%s%nTo:%s%nMessage:%s", UserExperience.username, receiver, message);
+			if (UserExperience.username != null) {
+				out.write(mess);
+				out.println();
+				out.close();
 			}
 		}
 	}
+
 	public static void seeAmessage() throws IOException, ClassNotFoundException {
 		BufferedReader out = new BufferedReader(new FileReader("C:\\Users\\User\\Desktop\\chat.txt"));
 		Files.filesToLists(out, messages);
 		ArrayList<String> texts = new ArrayList<String>();
-		String textReceiver=null;
-		String textMessage=null;
-		String textSender=null;
+		String textReceiver = null;
+		String textMessage = null;
+		String textSender = null;
 		for (int i = 0; i < messages.size(); i++) {
 			if (messages.get(i).contains("From:") == true
 					&& messages.get(i).contains("From:" + UserExperience.username) == false
 					&& messages.get(i).contains("Message:") == false) {
 				textSender = messages.get(i);
 			}
-			if (textSender != UserExperience.username)
-			{
+			if (textSender != UserExperience.username) {
+				;
 				if (messages.get(i).contains("Message:") == true && textSender != null) {
 					textMessage = messages.get(i);
 					String message1 = String.format("%s%n%s", textSender, textMessage);
@@ -80,14 +84,19 @@ public class Message {
 			int answer = rew.nextInt();
 			if (answer == 1) {
 				Like.likeAmessge(answer);
+				Files.notification.write(texts.get(j)+"-->liked");
+				Files.notification.println();
 			}
 			if (answer == 2) {
 				Like.dislikeAmessage(answer);
+				Files.notification.write(texts.get(j)+"-->disliked");
+				Files.notification.println();
 			}
 		}
 		if(texts.isEmpty()==true) {
 			System.out.println("No new messages!");
 		}
-		
+		Files.notification.close();
+		Files.notification.close();
 	}
 }
